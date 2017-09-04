9badba44d2600724ece6402928edf18242d55b0e
Michał Szczepańczyk <michal@szczepanczyk.com>
2017-09-04 11:28:15 +0200

    Create tests for PandasAttribute

--- taurus_pandas/test/test_pdsattribute.py
+++ taurus_pandas/test/test_pdsattribute.py
@@ -24,30 +24,105 @@
 #############################################################################
 
 """Tests for taurus.core.taurus_pandas.test.test_pdsattribute..."""
+
+import os
+
 import numpy
 import taurus
-from taurus.core import TaurusAttrValue
-from taurus.external.pint import Quantity
+from taurus.core import TaurusAttrValue, DataType
 from taurus.external import unittest
+from taurus.external.pint import Quantity
 from taurus.test import insertTest
-from taurus_pandas.pdsattribute import PandasAttribute
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 @insertTest(helper_name="read_attr",
-            attr_fullname='pds:/./res/file.xls::',
+            attr_fullname='pds:{}/res/file.csv::["int1"]'.format(BASE_DIR),
             expected=dict(
-                rvalue="sth"
-            )
-)
+                rvalue=Quantity([1, 2, 3], 'dimensionless'),
+                type=DataType.Integer
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["int1,"int2"]'.format(
+                BASE_DIR),
+            expected=dict(
+                rvalue=Quantity([[1, 4], [2, 5], [3, 6]], 'dimensionless'),
+                type=DataType.Integer
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["float1"]'.format(BASE_DIR),
+            expected=dict(
+                rvalue=Quantity([1.2, 3.4, 5.6], 'dimensionless'),
+                type=DataType.Float
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["float1","float2"]'.format(
+                BASE_DIR),
+            expected=dict(
+                rvalue=Quantity([[1.2, 1.2], [3.4, 2.3], [5.6, 3.4]],
+                                'dimensionless'),
+                type=DataType.Float
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["string1"]'.format(BASE_DIR),
+            expected=dict(
+                rvalue=['a', 'b', 'c'],
+                type=DataType.String
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["string1","string2"]'.format(
+                BASE_DIR),
+            expected=dict(
+                rvalue=[['a', 'd'], ['b', 'e'], ['c', 'f']],
+                type=DataType.String
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["bool1"]'.format(BASE_DIR),
+            expected=dict(
+                rvalue=[True, True, False],
+                type=DataType.Boolean
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["bool1","bool2"]'.format(
+                BASE_DIR),
+            expected=dict(
+                rvalue=[[True, False], [True, False], [False, True]],
+                type=DataType.Boolean
+            ))
+# Mixed types
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["int1","float1"]'.format(
+                BASE_DIR),
+            expected=dict(
+                rvalue=Quantity([[1, 1.2], [2, 3.4], [3, 5.6]],
+                                'dimensionless'),
+                type=DataType.Float
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::["int1","string1"]'.format(
+                BASE_DIR),
+            expected=dict(
+                rvalue=[['1', 'a'], ['2', 'b'], ['3', 'c']],
+                type=DataType.String
+            ))
+@insertTest(helper_name="read_attr",
+            attr_fullname='pds:{}/res/file.csv::'.format(
+                BASE_DIR),
+            expected=dict(
+                rvalue=[['1', '4', '1.2', '1.2', 'a', 'd', 'True', 'False'],
+                        ['2', '5', '3.4', '2.3', 'b', 'e', 'True', 'False'],
+                        ['3', '6', '5.6', '3.4', 'c', 'f', 'False', 'True']],
+                type=DataType.String
+            ))
 class PandasAttributeTestCase(unittest.TestCase):
-
     def read_attr(self, attr_fullname, expected={}):
         a = taurus.Attribute(attr_fullname)
         read_value = a.read()
 
         msg = ('read() for "{}" did not return an TaurusAttrValue ' +
                '(got a {})'.format(attr_fullname,
-                                   read_value.__class__.name__))
+                                   read_value.__class__.__name__))
         self.assertTrue(isinstance(read_value, TaurusAttrValue), msg)
 
         # Test attribute
@@ -55,7 +130,7 @@ class PandasAttributeTestCase(unittest.TestCase):
             try:
                 got = getattr(a, k)
             except AttributeError:
-                msg = ('The attribute "{}" does not provide infor on {}'
+                msg = ('The attribute "{}" does not provide info on {}'
                        .format(attr_fullname, k))
                 self.fail(msg)
             msg = ('{} for "{}" should be {} (got {})'.format(
@@ -79,4 +154,3 @@ class PandasAttributeTestCase(unittest.TestCase):
             chk = bool(got == exp)
 
         self.assertTrue(chk, msg)
-
